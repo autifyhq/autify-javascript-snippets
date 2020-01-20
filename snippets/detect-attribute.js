af125ad1fa2ad3cbc8fd504c5debcab48dc44baf
Sam Yamashita <1587053+sotayamashita@users.noreply.github.com>
2020-01-20 20:00:36 +0900

    Update detect-attribute.js

--- snippets/detect-attribute.js
+++ snippets/detect-attribute.js
@@ -1,12 +1,22 @@
-const query = "<TODO: Some locator>"
-const element = document.querySelector(query)
+// Locate the element
+var selector = "<TODO: REPLACE SELECTOR>"
+var element  = document.querySelector(selector)
+
+// Stop process if it does not exist
 if (!element) {
-  throw new Error('Cannot find the element')
+  throw new Error('Error: cannot find the element with ' + selector + '.');
 }
 
-const attrName = "<TODO: Attribute Name>"
+// Set attribute name
+const attrName = "<TODO: REPLACE ATTRIBUTE NAME. e.g) id, class, data->"
+
+// Get value from the elment
 const actual   = element.getAttribute(attrName)
-const expected = "<TODO: Expected value>"
+
+// Set expected value
+const expected = "<TODO: REPLACE EXPECTED VALUE>"
+
+// Stop process if actual and expected are different
 if (actual !== expected) {
-   thow new Error('Actual:' + actual + ", Expected:" + expected)
+   thow new Error('Error: expected(' + expected + ') and actual(' + actual + ') are not same.')
 }
