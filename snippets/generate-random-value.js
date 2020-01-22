44e848ec77a3fbea226ee38d04c219f2b75fbba9
Sam Yamashita <1587053+sotayamashita@users.noreply.github.com>
2020-01-22 15:26:37 +0900

    Create generate-random-value.js

---
+++ snippets/generate-random-value.js
@@ -0,0 +1,8 @@
+// Date
+// 日付
+return new Date().toString()
+
+// Random number
+// 乱数
+var size = "<TODO: REPLACE SIZE>"
+return Math.floor(Math.random() * (9 * Math.pow(10, n - 1))) + Math.pow(10, n - 1)
