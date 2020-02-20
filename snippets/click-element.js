4001906b3ce828d0826906632d0d81d61e5783fb
macoril <macoriloid@gmail.com>
2020-02-20 22:02:24 +0900

    added semicolon to avoid warnings, and changed comment style
    `//` style has a problem on IE
    https://autifyhq.slack.com/archives/CGQAD7KPU/p1582081992010200?thread_ts=1569631610.000200&cid=CGQAD7KPU
    
    Signed-off-by: macoril <macoriloid@gmail.com>

--- snippets/click-element.js
+++ snippets/click-element.js
@@ -1,14 +1,14 @@
-// Locate the element
-// 要素を探す
-var selector = "<TODO: REPLACE SELECTOR>"
-var element  = document.querySelector(selector)
+/* Locate the element
+ * 要素を探す */
+var selector = "<TODO: REPLACE SELECTOR>";
+var element  = document.querySelector(selector);
 
-// Stop process if it does not exist
-// 要素がなければ処理を中断する
+/* Stop process if it does not exist
+ * 要素がなければ処理を中断する */
 if (!element) {
   throw new Error('Error: cannot find the element with selector(' + selector + ').');
 }
 
-// Fire a click event
-// クリックする
-element.click()
+/* Fire a click event
+ * クリックする */
+element.click();
--- snippets/generate-random-value.js
+++ snippets/generate-random-value.js
@@ -1,8 +1,15 @@
-// Date
-// 日付
-return new Date().toString()
+/* Date
+ * 日付 */
+/* change the timezone if you need
+ * 日本時間(JST)の場合 9 */
+var to_timezone = 9;
 
-// Random number
-// 乱数
-var size = "<TODO: REPLACE SIZE>"
-return Math.floor(Math.random() * (9 * Math.pow(10, n - 1))) + Math.pow(10, n - 1)
+var from_timezone_offset = new Date().getTimezoneOffset() * 60 * 1000;
+var to_timezone_offset = 9 * 60 * 60 * 1000;
+var date = new Date(Date.now() + from_timezone_offset + to_timezone_offset);
+return date;
+
+/* Random number
+ * 乱数 */
+var size = "<TODO: REPLACE SIZE>";
+return Math.floor(Math.random() * (9 * Math.pow(10, n - 1))) + Math.pow(10, n - 1);
