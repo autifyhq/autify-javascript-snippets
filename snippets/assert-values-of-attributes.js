187cd480cd0f7e43f7240e666117ddff0b5352e2
macoril <macoriloid@gmail.com>
2020-04-13 14:52:52 +0900

    update snippet, adding a note on the top of it to explain the usage.
    gathered values on the top which user needs to set

--- snippets/assert-values-of-attributes.js
+++ snippets/assert-values-of-attributes.js
@@ -1,6 +1,37 @@
+/* Usage of this snippet:
+ *   In the case when you want to assert a value of a specified attribute.
+ *
+ *   Change the values written below.
+ *    selector: A string of a selector to pinpoint the element.
+ *    attrName: A string of the attribute which you want to assert its value.
+ *    expected: A string of the your expected value.
+ *    errMessage:
+ *      A string of the error message which you want to show when it fails.
+ *      It will be a default string if you don't change from `<TODO: REPLACE>`.
+ *
+ * このスニペットの使い方:
+ *   特定の属性の値を検証したいときにご利用ください。
+ *
+ *   以下の値を変更してください
+ *    selector: 対象要素を特定するセレクタの文字列
+ *    attrName: 値を検証したい属性の文字列
+ *    expected: 検証対象属性に期待する値の文字列
+ *    errMessage:
+ *      検証が失敗したときに表示したいエラーメッセージの文字列
+ *      `<TODO: REPLACE>` のまま変更がない場合はデフォルトの文言で表示します。
+ */
+
+var selector = "<TODO: REPLACE>";
+var attrName = "<TODO: REPLACE>"; /* e.g) id, class, data */
+var expected = "<TODO: REPLACE>";
+var errMessage = "<TODO: REPLACE>";
+
+
+/* --------- You don't need to touch below ---------------
+ * --------- ここから下は変える必要はありません ---------- */
+
 /* Locate the element
  * 要素を探す */
-var selector = "<TODO: REPLACE SELECTOR>";
 var element  = document.querySelector(selector);
 
 /* Stop process if it does not exist
@@ -9,20 +40,16 @@ if (!element) {
   throw new Error('Error: cannot find the element with selector(' + selector + ').');
 }
 
-/* Set attribute name
- * 探したい属性を設定する */
-var attrName = "<TODO: REPLACE ATTRIBUTE NAME. e.g) id, class, data->";
-
 /* Get value from the elment
  * 属性の値を取得する */
 var actual   = element.getAttribute(attrName);
 
-/* Set expected value
- * 期待する値を設定する */
-var expected = "<TODO: REPLACE EXPECTED VALUE>";
-
 /* Stop process if actual and expected are different
  * 実際のものと期待するものが違う場合処理を中断する */
 if (actual !== expected) {
-   throw new Error('Error: expected(' + expected + ') and actual(' + actual + ') are not same.');
+  if (errMessage == "<TODO: REPLACE>") {
+    errMessage = 'Error: expected(' + expected + ') and actual(' + actual + ') are not same.');
+  }
+
+  throw new Error(errMessage);
 }
