5de97cf5ded6a140c2661a429a65b5bd053a0bca
macoril <macoriloid@gmail.com>
2021-06-03 22:17:43 +0900

    update explanation

--- snippets/generate-random-value.js
+++ snippets/generate-random-value.js
@@ -1,6 +1,14 @@
-/**
- * Random number
- * 乱数
+/* Usage of this snippet:
+ *   Use this snippet to create a random number.
+ * 
+ *   Change the following values to fit your test case:
+ *     digit: Number of digits of random number
+ *
+ * このスニペットの用途:
+ *   乱数を生成したいときにご利用ください。
+ * 
+ *   以下の値をテストケースに合うように変更してください:
+ *     digit: 生成したい乱数の桁数
  */
-var digit = "<TODO: REPLACE DIGIT>";
+var digit = 10; /* TODO: REPLACE NUMBER */
 return Math.floor(Math.random() * (9 * Math.pow(10, digit - 1))) + Math.pow(10, digit - 1);
