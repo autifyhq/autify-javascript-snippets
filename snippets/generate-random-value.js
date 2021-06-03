e49ce266e1c5c0b9948f763c51aca773dd27baa2
macoril <macoriloid@gmail.com>
2021-06-03 22:06:10 +0900

    cropped timestamp part and created new file

--- snippets/generate-random-value.js
+++ snippets/generate-random-value.js
@@ -1,22 +1,4 @@
 /**
- * Date
- * 日付
- * 
- * Since the Autify test run cannot be certain of which time zone the server is in, you need to calculate the offset.
- * Autifyのテスト実行ではどのタイムゾーンのサーバーか確約ができないため、オフセットを計算する必要があります。
- * 
- * If you want to get Japan time.
- * 日本時間(JST)を取得したい場合
- * var to_timezone = 9;
- */
-var to_timezone = <TODO: REPLACE SIZE>;
-
-var from_timezone_offset = new Date().getTimezoneOffset() * 60 * 1000;
-var to_timezone_offset = to_timezone * 60 * 60 * 1000;
-var date = new Date(Date.now() + from_timezone_offset + to_timezone_offset);
-return date;
-
-/**
  * Random number
  * 乱数
  */
