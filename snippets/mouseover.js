20c17a04a05916598e8d6c7337f85affc87a7327
FujiHaruka <fizzyjohn1427@gmail.com>
2021-10-19 14:57:21 +0900

    Enable to scroll tooltip into view after mouseover

--- snippets/mouseover.js
+++ snippets/mouseover.js
@@ -7,34 +7,48 @@
  */
 
 /**
- * Locate the element
- * 要素を探す
+ * CSS selector to locate the element of the mouseover target
+ * マウスオーバー対象の要素を特定するための CSS セレクタ
  */
 var selector = "<TODO: REPLACE SELECTOR>";
-var element  = document.querySelector(selector);
+
+/**
+ * (Optional) CSS selector to locate the element like a tooltip that will appear by hovering the mouse over the target element.
+ * This is used to scroll the tooltip into view after mouseover.
+ * (任意) 上記の要素にマウスオーバーすると現れるツールチップ要素に対する CSS セレクタ
+ * このセレクタを指定すると、マウスオーバー後にツールチップ要素が画面中央に来るようスクロールします
+ */
+var tooltipSelector = "";
+
+/* --------- You don't need to touch below ---------------
+ * --------- ここから下は変える必要はありません ---------- */
+
+var element = document.querySelector(selector);
 
 /**
  * Stop process if it does not exist
  * 要素がなければ処理を中断する
  */
 if (!element) {
-  throw new Error('Error: cannot find the element with selector(' + selector + ').');
+  throw new Error(
+    "Error: cannot find the element with selector(" + selector + ")."
+  );
 }
 
 var event;
-if (typeof(Event) === 'function') {
-   /**
-    * For modern browser
-    * モダンブラウザの場合
-    */
-   event = new Event('mouseover');
+if (typeof Event === "function") {
+  /**
+   * For modern browser
+   * モダンブラウザの場合
+   */
+  event = new Event("mouseover");
 } else {
-   /**
-    * For IE 11
-    * IE 11 の場合
-    */
-   event = document.createEvent('Event');
-   event.initEvent('mouseover', true, true);
+  /**
+   * For IE 11
+   * IE 11 の場合
+   */
+  event = document.createEvent("Event");
+  event.initEvent("mouseover", true, true);
 }
 
 /**
@@ -42,3 +56,18 @@ if (typeof(Event) === 'function') {
  * マウスオーバーイベントを発火させる
  */
 element.dispatchEvent(event);
+
+/**
+ * Scroll the tooltip into view if `tooltipSelector` is specified
+ * `tooltipSelector` が指定されていたらツールチップ要素が画面中央にくるようスクロールする
+ */
+if (tooltipSelector) {
+  var tooltip = document.querySelector(tooltipSelector);
+  if (!tooltip) {
+    throw new Error(
+      "Error: cannot find the element with selector(" + tooltipSelector + ")."
+    );
+  }
+
+  tooltip.scrollIntoView();
+}
