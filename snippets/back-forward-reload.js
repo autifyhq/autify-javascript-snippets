c2591e63b502816112fdace33a0c498827f838fd
FujiHaruka <fizzyjohn1427@gmail.com>
2022-02-26 12:55:21 +0900

    now we can delete old contents

--- snippets/back-forward-reload.js
+++
@@ -1,8 +0,0 @@
-/* Back 戻る */
-window.history.back();
-
-/* Forward 進む */
-window.history.forward();
-
-/* Reload 更新 */
-location.reload();
--- snippets/scroll.js
+++
@@ -1,5 +0,0 @@
-/**
- * x-coord, X軸
- * y-coord, Y軸
- */
-window.scrollTo(x-coord, y-coord);
